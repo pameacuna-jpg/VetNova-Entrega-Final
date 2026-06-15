f1ed8529b98b135ffabe69a3fb7f563cb6ada36e
Pamelaacuna <pame.acuna@duocuc.cl>
2026-06-14 21:11:59 -0400

    Auditoria 4 - agrega OpenAPI y YAML en microservicios

--- gabriel-martinez-vetnova-microservicios-main/usuarios/src/main/java/com/vetnova/usuarios/UsuariosApplication.java
+++ gabriel-martinez-vetnova-microservicios-main/usuarios/src/main/java/com/vetnova/usuarios/UsuariosApplication.java
@@ -1,4 +1,5 @@
 package com.vetnova.usuarios;
+package com.vetnova.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
